d7eb6f591ad98689e80b18a63c9128c5737e05de
fone <fone@News-MBP.attlocal.net>
2022-10-26 21:19:35 -0700

    excav8r fix

--- get_images.py
+++ get_images.py
@@ -2,7 +2,33 @@ import argparse
 from excav8r import Excva8r
 import os
 from PIL import Image
-from app.backend.utils import randomize_mtime, gallery_as_path_list
+from random import randrange
+from datetime import datetime
+import piexif
+
+def gallery_as_path_list(path):
+    p = []
+
+    for entry in os.listdir(path):
+        p.append(f'{path}/{entry}')
+
+    return p
+
+
+def randomize_mtime(path):
+    month = randrange(1,12)
+    day = randrange(1,30)
+    year = randrange(2000,2020)
+    d = datetime(year, month, day, 0, 0)
+    epoch = d.timestamp()
+    os.utime(path, (epoch, epoch))
+    exif_dict = piexif.load(path)
+    new_date = datetime(year, month, day, 0, 0).strftime("%Y:%m:%d %H:%M:%S")
+    exif_dict['0th'][piexif.ImageIFD.DateTime] = new_date
+    exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = new_date
+    exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = new_date
+    exif_bytes = piexif.dump(exif_dict)
+    piexif.insert(exif_bytes, path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
